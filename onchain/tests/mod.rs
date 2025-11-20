8d92d24e15acf82cbe441f6fd8035433a571a78c
Fernando Medeiros <fernandogdemedeirosw@gmail.com>
2025-11-20 10:01:35 -0300

    tests: initial setup

--- onchain/tests/mod.rs
+++ onchain/tests/mod.rs
@@ -1,1 +1,11 @@
-fn setup() {}
+use litesvm::LiteSVM;
+
+struct TestSetup {
+    pub lite_svm: LiteSVM,
+}
+
+fn setup() -> TestSetup {
+    TestSetup {
+        lite_svm: LiteSVM::new().with_default_programs(),
+    }
+}
