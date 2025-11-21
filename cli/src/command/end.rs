bb2fa99faaabf4db8edc9c95761a360b3e00610d
Fernando Medeiros <fernandogdemedeirosw@gmail.com>
2025-11-21 14:53:02 -0300

    feat: integrate end prediction instruction

--- cli/src/command/end.rs
+++ cli/src/command/end.rs
@@ -1,8 +1,12 @@
 use {
     super::{CommandContext, RunCommand},
-    crate::CliResult,
+    crate::{CliResult, PROGRAM_ID},
+    solana_client::rpc_config::UiTransactionEncoding,
+    solana_message::{v0::Message, AccountMeta, Instruction, VersionedMessage},
     solana_pubkey::Pubkey,
-    tracing::info,
+    solana_signer::Signer,
+    solana_transaction::versioned::VersionedTransaction,
+    tracing::{error, info},
 };
 
 pub struct EndCommand {
@@ -17,11 +21,54 @@ impl EndCommand {
 }
 
 impl RunCommand for EndCommand {
-    fn run(&self, _context: CommandContext) -> CliResult<()> {
+    fn run(&self, context: CommandContext) -> CliResult<()> {
         info!("Ending prediction market...");
-        info!("Market: {}", self.market);
-        info!("Winner: {}", self.winner);
-        info!("This command is not yet implemented");
+
+        // Creator must be signer in order to end the market
+        let creator_account = context.keypair.pubkey();
+
+        // Discriminator, winner option
+        let instruction_data = vec![2, self.winner];
+
+        let accounts = vec![
+            AccountMeta::new(creator_account, true),
+            AccountMeta::new(self.market, false),
+        ];
+
+        let end_prediction_ix =
+            Instruction::new_with_bytes(PROGRAM_ID, &instruction_data, accounts);
+
+        let transaction = VersionedTransaction::try_new(
+            VersionedMessage::V0(
+                Message::try_compile(
+                    &creator_account,
+                    &[end_prediction_ix],
+                    &[],
+                    context
+                        .client
+                        .get_latest_blockhash()
+                        .expect("Failed to fetch latest blockhash"),
+                )
+                .expect("Failed to build VersionedMessage"),
+            ),
+            &[context.keypair],
+        )
+        .expect("Failed to build versioned transaction");
+
+        match context.client.send_transaction_with_config(
+            &transaction,
+            solana_client::rpc_config::RpcSendTransactionConfig {
+                encoding: Some(UiTransactionEncoding::Base64),
+                ..Default::default()
+            },
+        ) {
+            Ok(_) => info!(
+                "Prediction {} successfully ended, winner is {} !",
+                self.market, self.winner
+            ),
+            Err(e) => error!("Prediction settle failed for {}, error: {}", self.market, e),
+        }
+
         Ok(())
     }
 }
